75f4cd55c1673ecb81213b8ce054ee798bd07489
j-mak7 <juliamakarowaa@yandex.ru>
2026-05-08 12:51:38 +0300

    final version

--- task9/9.c
+++ task9/9.c
@@ -7,17 +7,27 @@ void max_subarray(int* arr, int arr_len) {
 	int max_len = 1;
 	int start = 0;
 	int end = 0;
+	int max_sum = arr[0];
+	int sub_sum = arr[0];
 	for (int i = 0; i < arr_len - 1; i++){
 		if (arr[i] <= arr[i + 1]){
 			sub_len++;
+			sub_sum += arr[i+1];
+			//printf("sub_sum: %d max_sum: %d\n", sub_sum, max_sum);
 			if (sub_len > max_len){
 				max_len = sub_len;
+				start = i + 2 - sub_len;
+				end = i + 2;
+			}
+			else if (sub_len == max_len && sub_sum > max_sum) {
+				max_sum = sub_sum;
 				start = i + 2 - sub_len;
 				end = i + 2;
 			}
 		}
 		else {
 			sub_len = 1;
+			sub_sum = arr[i + 1];
 		}
 	}
 	for (int i = start; i < end; i++) {
@@ -28,18 +38,18 @@ void max_subarray(int* arr, int arr_len) {
 }
 
 int main() {
-        char *line = NULL;
-        size_t len = 0;
-        int n_read = getline(&line, &len, stdin);
-        if (n_read > 0 && line[n_read-1] == '\n') {
-                line[n_read-1] = '\0';
-        }
-        int len_arr = 0;
-        int* arr_dig = str_to_arr(line, &len_arr);
-        if (arr_dig) {
-                max_subarray(arr_dig, len_arr);
-                free(arr_dig);
-        }
-        free(line);
-        return 0;
+	char *line = NULL;
+	size_t len = 0;
+	int n_read = getline(&line, &len, stdin);
+	if (n_read > 0 && line[n_read-1] == '\n') {
+		line[n_read-1] = '\0';
+	}
+	int len_arr = 0;
+	int* arr_dig = str_to_arr(line, &len_arr);
+	if (arr_dig) {
+		max_subarray(arr_dig, len_arr);
+		free(arr_dig);
+	}
+	free(line);
+	return 0;
 }
