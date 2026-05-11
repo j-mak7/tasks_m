266045647e777e5953e91e855443132c6f385f59
j-mak7 <juliamakarowaa@yandex.ru>
2026-05-11 12:53:11 +0300

    errno

--- task5/5.c
+++ task5/5.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
 int main(void) {
 	int fd = open("file.txt", O_RDONLY);
 	if (fd == -1) {
 		perror("Can’t open file");
+		printf("Errno = %s\n", strerrorname_np(errno));
 	}
 	close(fd);
 	return 0;
