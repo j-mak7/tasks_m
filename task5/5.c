825424218f05879c84f531ae05835362051379ee
j-mak7 <juliamakarowaa@yandex.ru>
2026-05-11 12:56:39 +0300

    final version

--- task5/5.c
+++ task5/5.c
@@ -3,12 +3,15 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(void) {
 	int fd = open("file.txt", O_RDONLY);
 	if (fd == -1) {
 		perror("Can’t open file");
 		printf("Errno = %s\n", strerrorname_np(errno));
+		printf("sys_errlist: %s\n", strerror(errno));
+		exit(errno);
 	}
 	close(fd);
 	return 0;
