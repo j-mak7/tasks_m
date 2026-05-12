f9ebfbf8bafd2e3730c8a5ee7cf5fd8605b73435
j-mak7 <juliamakarowaa@yandex.ru>
2026-05-12 18:44:52 +0300

    parent_copy and child_copy

--- task7/7.c
+++ task7/7.c
@@ -10,9 +10,21 @@ int main(){
 		exit(1);
 	}
 	else if (p == 0) {
+		FILE* child_f = fopen("child_copy", "w+");
+		if (!child_f) {
+			printf("Error when opening child_copy\n");
+			exit(1);
+		}
+		fclose(child_f);
 		printf("Child\n");
 	}
 	else {
+		FILE* parent_f = fopen("parent_copy", "w+");
+		if (!parent_f) {
+			printf("Error when opening parent_copy\n");
+			exit(1);
+		}
+		fclose(parent_f);
 		printf("Parrent\n");
 	}
 	return 0;
